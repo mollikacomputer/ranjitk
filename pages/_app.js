f43b42633f0ec9d34273b3f599854b8404bbcc76
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-23 22:04:50 +0600

    comment section added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Footer from '@/components/Footer';
 import HeroSection from '@/components/HeroSection';
 import Navbar from '@/components/Navbar';
+import Comments from '@/components/comments';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
     <>
     <Navbar/>
     <HeroSection/>
+    <Comments/>
     <Component {...pageProps} />
     <Footer/>
     </>
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <h2 className="text-3xl"> Ranjit Home page</h2>
-      <button className="btn btn-secondary">Button</button>
+      
     </main>
   )
 }
