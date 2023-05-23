1027f8c6125c551ad803d4e05122b6409a4615dd
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-23 22:06:17 +0600

    add some rename

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Footer from '@/components/Footer';
 import HeroSection from '@/components/HeroSection';
 import Navbar from '@/components/Navbar';
-import Comments from '@/components/comments';
+import Comments from '@/components/Comments';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
