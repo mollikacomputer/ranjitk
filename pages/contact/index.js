deb900f38c606180d8c58887b9f31c8633d3896b
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-26 17:40:01 +0600

    email js local host done

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,13 +1,73 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-const index = () => {
+const Contact = () => {
+
+    // for email js
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_6cbaf0b",
+        "template_39szbht",
+        form.current,
+        "QZ-FwUQStvQDi1wdO"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+
   return (
-    <div>
-      <div className="hero min-h-screen bg-base-200">
+    <div >
+        {/* <h2 className="text-3xl">EmailJS</h2>
+      <p>Sent mail without server code</p>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Sent Your Request</h2>
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input
+              type="text"
+              name="user_name"
+              className="input input-error w-full max-w-xs"
+            />
+            <label>Email</label>
+            <input
+              type="email"
+              name="user_email"
+              className="input input-bordered input-error w-full max-w-xs"
+            />
+            <label>Message</label>
+            <textarea
+              name="message"
+              className="input input-bordered input-error w-full max-w-xs"
+            />
+            <div className="card-actions justify-start">
+              <button type="submit" value="Send" className="btn">
+                Sent SMS
+              </button>
+            </div>
+          </form>
+        </div>
+      </div> */}
+
+
+       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
         <img src="./contact_form.jpg" className="max-w-sm rounded-lg shadow-2xl" />
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body">
+            <form ref={form} onSubmit={sendEmail} className="card-body">
                 <h2 className="text-2xl">Sent Your Message</h2>
               <div className="form-control">
                 <label className="label">
@@ -16,6 +76,7 @@ const index = () => {
                 <input
                   type="text"
                   placeholder="name"
+                  name="user_name"
                   className="input input-bordered"
                 />
               </div>
@@ -24,30 +85,26 @@ const index = () => {
                   <span className="label-text">Email</span>
                 </label>
                 <input
-                  type="text"
+                  type="email"
                   placeholder="email"
-                  className="input input-bordered"
+                  name="user_email"
+                  className="input input-bordered "
                 />
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Message</span>
+                <textarea name="message" placeholder="Bio" className="textarea textarea-bordered textarea-lg w-full max-w-xs" ></textarea>
                 </label>
-                <input
-                  type="text"
-                  placeholder="Message"
-                  className="input input-bordered"
-                />
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Sent Message</button>
+                <button type="submit" value="Send" className="btn btn-primary">Sent Message</button>
               </div>
-            </div>
+            </form>
           </div>
         </div>
-      </div>
+      </div> 
     </div>
   );
 };
 
-export default index;
+export default Contact;
