b598386ccfd492d0c40d3900289aeef094b711c6
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-27 19:43:03 +0600

    sensor add for count up

--- components/CountUpPage.js
+++ components/CountUpPage.js
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { CountUp } from 'use-count-up';
 import Styles from "@/styles/countuppage.module.css";
+import ScrollTrigger from "react-scroll-trigger";
 
 const CountUpPage = () => {
 
+  const [counterOn, setCounterOn] = useState(false);
 
   return (
+  <ScrollTrigger onEnter={()=> setCounterOn(true)} onExit={()=> setCounterOn(false)} >
     
     <div className={Styles.container}>
       <div className={Styles.wrapper}>
         <div className={Styles.countupstyle}>
             <span className={Styles.num}>
-            <CountUp isCounting end={1320} duration={10} />  
+            {counterOn && <CountUp isCounting start={0} end={1320} duration={3.2} /> }  
             </span>  
             <p className={Styles.text}> 1st stage  </p>
         </div>
       </div>
       <div className={Styles.wrapper}>
         <div className={Styles.countupstyle}>
-            <span className={Styles.num} >
-            <CountUp isCounting end={2052} duration={10} /> 
+            <span className={Styles.num} > 
+            {counterOn && <CountUp isCounting start={0} end={2502} duration={3.2} /> }
             </span>  
             <p className={Styles.text}> 2nd stage  </p>
         </div>
@@ -27,7 +30,7 @@ const CountUpPage = () => {
       <div className={Styles.wrapper}>
         <div className={Styles.countupstyle}>
             <span className={Styles.num}>
-            <CountUp isCounting end={55555} duration={10} /> 
+            {counterOn && <CountUp isCounting start={0} end={5554} duration={3.2} /> }
             </span>  
             <p className={Styles.text}> 3rd stage  </p>
         </div>
@@ -35,14 +38,14 @@ const CountUpPage = () => {
       <div className={Styles.wrapper}>
         <div className={Styles.countupstyle}>
             <span className={Styles.num} >
-            <CountUp isCounting end={6666} duration={10} /> 
+            {counterOn && <CountUp isCounting start={0} end={6665} duration={3.2} /> }
             </span>  
             <p className={Styles.text}> 4th stage  </p>
         </div>
       </div>
       {/* {firstTest()} */}
     </div>
-    
+  </ScrollTrigger>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import Footer from '@/components/Footer';
-import HeroSection from '@/components/HeroSection';
-import Navbar from '@/components/Navbar';
-import Comments from '@/components/Comments';
 import '@/styles/globals.css';
-import CountUpPage from '@/components/CountUpPage';
+import Navbar from '@/components/Navbar';
+
 
 
 export default function App({ Component, pageProps }) {
